34789d22c308e74a98fce5425fdb16537c57d321
unknown <kirichenko723@gmail.com>
2021-01-22 13:18:15 +0300

    Some minor changes in Class Components

--- src/components/TilesGame/Modal/Modal.jsx
+++ src/components/TilesGame/Modal/Modal.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import classes from './Modal.module.css';
+import React from "react";
+import classes from "./Modal.module.css";
 
-
-const Modal = ({initializeGame, movesCount}) => (
-    <div className={classes.wrapper}>
-        <div className={classes.modalWindow}>
-            <h1 className={classes.congratulation}>Congratulations, you won in {movesCount} rounds!</h1>
-            <button className={classes.button} onClick={()=> {initializeGame()}}>Play again</button>
-        </div>
+const Modal = ({ initializeGame, movesCount }) => (
+  <div className={classes.wrapper}>
+    <div className={classes.modalWindow}>
+      <h1 className={classes.congratulation}>
+        Congratulations, you won in {movesCount} rounds!
+      </h1>
+      <button className={classes.button} onClick={initializeGame}>
+        Play again
+      </button>
     </div>
-)
+  </div>
+);
 
 export default Modal;
--- src/components/TilesGame/TilesGameContainer.js
+++ src/components/TilesGame/TilesGameContainer.js
@@ -1,80 +1,111 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { getRandomElements, shuffleArr } from '../../common/array-handlers';
-import { initializeGame, setCardOpened, setCardsSolved, setCardsClosed, roundFinished, finishTheGame } from '../../redux/tile-game-reducer'
-import TilesGame from './TilesGame';
+import React from "react";
+import { connect } from "react-redux";
+import { getRandomElements, shuffleArr } from "../../common/array-handlers";
+import {
+  initializeGame,
+  setCardOpened,
+  setCardsSolved,
+  setCardsClosed,
+  roundFinished,
+  finishTheGame,
+} from "../../redux/tile-game-reducer";
+import TilesGame from "./TilesGame";
 
 //This container component connects our UI of Tiles Game to Redux store and processing whole game logic to keep our presentational component clean
 //This component can be refactored to Functional component with useState and useEffect hooks
 //If needed all logical methods can be moved to separated modules
 
 class TilesGameContainer extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            firstCard: null,//This local state is needed to store temporary data about selected cards. This data also can be stored in global Redux store
-            secondCard: null
-        }
-    }
-    prepareCards = () => {
-        let cardsContent = getRandomElements(this.props.fieldHeight * this.props.fieldWidth / 2, this.props.cardsImages); //Choosing random pictures
-        cardsContent = shuffleArr(cardsContent.concat(cardsContent));
-        let cardsForGame = [];//Creating an array of cards for the game with random pictures
-        for (let i = 0; i < cardsContent.length; i++) {
-            cardsForGame.push({ content: cardsContent[i], id: i, opened: false, solved: false })
-        }
-        this.props.initializeGame(cardsForGame);
-    }
-    handleRound = () => {//Logic, that is provided when second picture was picked
-        if (this.props.cards[this.state.firstCard].content === this.props.cards[this.state.secondCard].content) {
-            this.props.setCardsSolved(this.state.firstCard, this.state.secondCard);
-            this.setState({ firstCard: null, secondCard: null });
-            this.props.roundFinished(true)
-        } else {
-            this.props.roundFinished(false);
-            this.props.setCardsClosed(this.state.firstCard, this.state.secondCard);
-            this.setState({ firstCard: null, secondCard: null });
-        }
-    }
-    openCard = (cardId) => {
-        if (this.state.firstCard === null) {
-            this.setState({ firstCard: cardId });
-            this.props.setCardOpened(cardId);
-        } else {
-            if (this.state.secondCard === null && cardId !== this.state.firstCard) {
-                this.setState({ secondCard: cardId });
-                this.props.setCardOpened(cardId);
-                setTimeout(() => { this.handleRound() }, 700)//Delay of round processing can be changed here
-            }
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstCard: null, //This local state is needed to store temporary data about selected cards. This data also can be stored in global Redux store
+      secondCard: null,
+    };
+  }
 
-    componentDidMount() {
-        this.prepareCards();
+  prepareCards = () => {
+    let cardsContent = getRandomElements(
+      (this.props.fieldHeight * this.props.fieldWidth) / 2,
+      this.props.cardsImages
+    ); //Choosing random pictures
+    cardsContent = shuffleArr(cardsContent.concat(cardsContent));
+    let cardsForGame = []; //Creating an array of cards for the game with random pictures
+    for (let i = 0; i < cardsContent.length; i++) {
+      cardsForGame.push({
+        content: cardsContent[i],
+        id: i,
+        opened: false,
+        solved: false,
+      });
     }
-    componentDidUpdate(prevProps) {
-        (prevProps.cardsSolved !== this.props.cardsSolved) && (this.props.cardsSolved === this.props.cards.length)
-        && this.props.finishTheGame() // Checking if all cards were solved
+    this.props.initializeGame(cardsForGame);
+  };
+
+  handleRound = () => {
+    //Logic, that is provided when second picture was picked
+    const { cards, roundFinished, setCardsClosed, setCardsSolved } = this.props;
+    const { firstCard, secondCard } = this.state;
+    if (cards[firstCard].content === cards[secondCard].content) {
+      setCardsSolved(firstCard, secondCard);
+      this.setState({ firstCard: null, secondCard: null });
+      roundFinished(true);
+    } else {
+      roundFinished(false);
+      setCardsClosed(firstCard, secondCard);
+      this.setState({ firstCard: null, secondCard: null });
     }
-    render() {
-        return (
-                <TilesGame
-                    cards={this.props.cards}
-                    openCard={this.openCard}
-                    movesCount={this.props.movesCount}
-                    gameIsFinished={this.props.gameIsFinished}
-                    initializeGame={this.prepareCards} />
-        )
+  };
+
+  openCard = (cardId) => {
+    if (this.state.firstCard === null) {
+      this.setState({ firstCard: cardId });
+      this.props.setCardOpened(cardId);
+    } else if (
+      this.state.secondCard === null &&
+      cardId !== this.state.firstCard
+    ) {
+      this.setState({ secondCard: cardId });
+      this.props.setCardOpened(cardId);
+      setTimeout(this.handleRound, 700); //Delay of round processing can be changed here
     }
+  };
+
+  componentDidMount() {
+    this.prepareCards();
+  }
+  componentDidUpdate(prevProps) {
+    prevProps.cardsSolved !== this.props.cardsSolved &&
+      this.props.cardsSolved === this.props.cards.length &&
+      this.props.finishTheGame(); // Checking if all cards were solved
+  }
+  render() {
+    return (
+      <TilesGame
+        cards={this.props.cards}
+        openCard={this.openCard}
+        movesCount={this.props.movesCount}
+        gameIsFinished={this.props.gameIsFinished}
+        initializeGame={this.prepareCards}
+      />
+    );
+  }
 }
 const mapStateToProps = (state) => ({
-    cards: state.tiles.cards,
-    cardsSolved: state.tiles.cardsSolved,
-    movesCount: state.tiles.movesCount,
-    gameIsFinished: state.tiles.gameIsFinished,
-    fieldHeight: state.tiles.fieldHeight,
-    fieldWidth: state.tiles.fieldWidth,
-    cardsImages: state.tiles.cardsImages
-})
-const mapDispatchToProps = { initializeGame, setCardOpened, setCardsSolved, setCardsClosed, finishTheGame, roundFinished }
-export default connect(mapStateToProps, mapDispatchToProps)(TilesGameContainer)
+  cards: state.tiles.cards,
+  cardsSolved: state.tiles.cardsSolved,
+  movesCount: state.tiles.movesCount,
+  gameIsFinished: state.tiles.gameIsFinished,
+  fieldHeight: state.tiles.fieldHeight,
+  fieldWidth: state.tiles.fieldWidth,
+  cardsImages: state.tiles.cardsImages,
+});
+const mapDispatchToProps = {
+  initializeGame,
+  setCardOpened,
+  setCardsSolved,
+  setCardsClosed,
+  finishTheGame,
+  roundFinished,
+};
+export default connect(mapStateToProps, mapDispatchToProps)(TilesGameContainer);
